876a52bf6b0df8bf7b22566ce071a4205ca1f948
VoidGl1tch <kostksuba@gmail.com>
2026-04-18 15:26:38 +0700

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-class TextModel:
-    def __init__(self):
-        self._text = ""
-
-    def set_text(self, text: str):
-        self._text = text
-
-    def get_text(self) -> str:
-        return self._text
